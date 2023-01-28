ed9ed374d3d266c2805b2ba51dbd8ba4fb43285a
Igor Papandinas <igor.papandinas@posteo.net>
2023-01-28 11:56:34 +0100

    first commit

---
+++ src/api/typology/routes/typology.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * typology router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::typology.typology');
---
+++ src/api/typology/services/typology.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * typology service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::typology.typology');
