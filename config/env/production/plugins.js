ae91b4509d234429eae1df56e8a596bf236184c1
Igor Papandinas <igor.papandinas@posteo.net>
2023-01-29 09:37:17 +0100

    task: Cloudinary env fixed

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -3,9 +3,9 @@ module.exports = ({ env }) => ({
     config: {
       provider: 'cloudinary',
       providerOptions: {
-        cloud_name: env(process.env.CLOUDINARY_NAME),
-        api_key: env(process.env.CLOUDINARY_KEY),
-        api_secret: env(process.env.CLOUDINARY_SECRET),
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
       },
       actionOptions: {
         upload: {},
